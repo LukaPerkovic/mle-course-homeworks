4f2c4080e61515796d253f9ee1a1df4e49ffab6c
LukaPerkovic <luka.m.perkovic@gmail.com>
2022-06-21 18:11:57 +0200

    created testing folder and skeleton testing file

---
+++ data_governance_hw/tests/test_forest.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+from forest import forest.ActualTreeClassifier
+
+
+class TestActualTreeClassifier(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	def setUp(self):
+		pass
+
+	def test_argparse(self):
+		pass
+
+	def test_data_load(self):
+		pass
+
+	def test_pipeline(self):
+		pass
+
+	def test_score(self):
+		pass
+
+	def test_feature_importance_plot(self):
+		pass
