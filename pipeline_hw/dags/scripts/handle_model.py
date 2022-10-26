2c8393fdbbf8bfe5c88e1228194c656aeebce666
LukaPerkovic <51373370+LukaPerkovic@users.noreply.github.com>
2022-10-26 10:24:28 +0200

    Update handle_model.py

--- pipeline_hw/dags/scripts/handle_model.py
+++ pipeline_hw/dags/scripts/handle_model.py
@@ -1,9 +1,10 @@
 import os
+import pickle
 
 MODEL_DIR = '/models/'
 
 def save_model(model_name):
-	pass
+	pickle.dump(model, open(os.path.join(MODEL_DIR, model_name), 'wb'))
 
 def load_model(model_name):
 	pass
