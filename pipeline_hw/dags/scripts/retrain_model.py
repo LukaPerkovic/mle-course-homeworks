ac821877c6be8add13b73dc2d4b0b33651ce4935
LukaPerkovic <51373370+LukaPerkovic@users.noreply.github.com>
2022-11-02 14:33:26 +0100

    Update retrain_model.py

--- pipeline_hw/dags/scripts/retrain_model.py
+++ pipeline_hw/dags/scripts/retrain_model.py
@@ -1,44 +1,20 @@
 import os
 import numpy as np
 from datetime import datetime
-import logging
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 
 from scripts.handle_data import load_data
 from scripts.handle_model import save_model
-
-
-DATA_DIR = '/opt/airflow/data/'
-
+from scripts.modeling import modeling
 
 def retrain_model():
 	df = load_data()
 
-	logging.warn(f'Database dimensions: {df.shape}')
-
-	cv = KFold(n_splits=10, random_state=1, shuffle=True)
-
-
-	model = RandomForestClassifier(
-		criterion='log_loss',
-		max_depth=15,
-		min_samples_split=3,
-		min_samples_leaf=1
-		)
-
-
-	scores = cross_val_score(model, df.drop('Cover_Type', axis=1), df.Cover_Type, scoring='accuracy', cv=cv, n_jobs=1, error_score='raise')
-
-	logging.warn(f'Prediction scores: {scores}')
-
-	percentage = str(int(round(np.mean(scores), 2) * 100))
+	model, percentage = modeling(df)
 
 	actual_date = datetime.date.today().strftime('%Y%m%d')
 	rtrained = 'rt'
-	filename = f'{model}_{trained}_{actual_date}_{percentage}'
+	filename = f'model_{trained}_{actual_date}_{percentage}'
 
 	save_model(filename)
 
