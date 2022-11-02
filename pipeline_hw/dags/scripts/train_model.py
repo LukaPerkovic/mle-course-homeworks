9b4dfcda87ff2c585dc4917f55b68bb7bbbe8c6e
LukaPerkovic <51373370+LukaPerkovic@users.noreply.github.com>
2022-11-02 14:32:56 +0100

    Update train_model.py

--- pipeline_hw/dags/scripts/train_model.py
+++ pipeline_hw/dags/scripts/train_model.py
@@ -3,39 +3,20 @@ import numpy as np
 from datetime import datetime
 import logging
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 
 from scripts.handle_file import read_file
 from scripts.handle_model import save_model
-
+from scripts.modeling import modeling
 
 def train_model():
 	df = read_file('data_batch_transformed.csv')
 
-	logging.warn(f'Database dimensions: {df.shape}')
 
-	cv = KFold(n_splits=10, random_state=1, shuffle=True)
-
-
-	model = RandomForestClassifier(
-		criterion='log_loss',
-		max_depth=15,
-		min_samples_split=3,
-		min_samples_leaf=1
-		)
-
-
-	scores = cross_val_score(model, df.drop('Cover_Type', axis=1), df.Cover_Type, scoring='accuracy', cv=cv, n_jobs=1, error_score='raise')
-
-	logging.warn(f'Prediction scores: {scores}')
-
-	percentage = str(int(round(np.mean(scores), 2) * 100))
+	model, percentage = modeling(df)
 
 	actual_date = datetime.date.today().strftime('%Y%m%d')
 	trained = 't'
-	filename = f'{model}_{trained}_{actual_date}_{percentage}'
+	filename = f'model_{trained}_{actual_date}_{percentage}'
 
 	save_model(filename)
 
